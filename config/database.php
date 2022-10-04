6b745edda707bae275503950a4963d32e74265a3
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-10-04 10:51:11 +0600

    Update config\database.php

--- config/database.php
+++ config/database.php
@@ -58,6 +58,13 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'dump' => [
+                'dump_binary_path' => 'C:\xampp\mysql\bin', // only the path, so without `mysqldump` or `pg_dump`
+                'use_single_transaction',
+                'timeout' => 60 * 5, // 5 minute timeout
+                'exclude_tables' => ['table1', 'table2'],
+                //'add_extra_option' => '--optionname=optionvalue', // for example '--column-statistics=0'
+            ],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
